700f8dcd01268d6fb976328b6da8f9436487fc29
dr-matt-smith <matt.smith@tudublin.ie>
2026-01-03 17:12:34 +0000

    moved nav into layout - simplies home page

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,8 +1,29 @@
 <script>
-	import { Styles } from '@sveltestrap/sveltestrap';
 	import favicon from '$lib/assets/favicon.svg';
+	import logo from '$lib/assets/logo.svg';
+
+	import {Styles} from '@sveltestrap/sveltestrap';
+	import {
+		Collapse,
+		NavbarToggler,
+		NavbarBrand,
+		Nav,
+		Navbar,
+		NavItem,
+		NavLink,
+		Dropdown,
+		DropdownToggle,
+		DropdownMenu,
+		DropdownItem,
+	} from '@sveltestrap/sveltestrap';
 
 	let { children } = $props();
+
+	let isOpen = $state(false);
+
+	function handleNavClick() {
+		isOpen = !isOpen
+	}
 </script>
 
 <svelte:head>
@@ -10,5 +31,39 @@
 	<Styles />
 </svelte:head>
 
+
+
+<!--see-->
+<!--https://sveltestrap.js.org/?path=/docs/components-navbar&#45;&#45;docs-->
+
+<Navbar color="dark" dark expand="md" container="md">
+	<NavbarBrand href="/">
+		<img src={logo} alt="module logo" width="100rem" />
+		My Great Website !
+	</NavbarBrand>
+
+	<NavbarToggler on:click={handleNavClick} />
+
+	<Collapse {isOpen} navbar expand="md">
+		<!--        <Collapse {isOpen} navbar expand="md" on:update={handleUpdate}>-->
+		<Nav class="ms-auto" navbar>
+			<NavItem>
+				<NavLink href="#components/">Components</NavLink>
+			</NavItem>
+			<NavItem>
+				<NavLink href="https://github.com/sveltestrap/sveltestrap">GitHub</NavLink>
+			</NavItem>
+			<Dropdown nav inNavbar>
+				<DropdownToggle nav caret>Options</DropdownToggle>
+				<DropdownMenu end>
+					<DropdownItem>Option 1</DropdownItem>
+					<DropdownItem>Option 2</DropdownItem>
+					<DropdownItem divider />
+					<DropdownItem>Reset</DropdownItem>
+				</DropdownMenu>
+			</Dropdown>
+		</Nav>
+	</Collapse>
+</Navbar>
 
 {@render children()}
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,74 +1,17 @@
 <script>
-    import {Button, Container} from '@sveltestrap/sveltestrap';
-
-    import logo from '$lib/assets/logo.svg';
-
-
     import {
-        Collapse,
-        NavbarToggler,
-        NavbarBrand,
-        Nav,
-        Navbar,
-        NavItem,
-        NavLink,
-        Dropdown,
-        DropdownToggle,
-        DropdownMenu,
-        DropdownItem,
+        Button,
+        Container,
         Form,
         FormGroup,
         Input,
         Label
     } from '@sveltestrap/sveltestrap';
 
-    let isOpen = $state(false);
-
-    function handleNavClick() {
-        isOpen = !isOpen
-    }
-
     function handleForm1ButtonClick() {
         alert("Ouch - you clicked me!")
     }
-
 </script>
-
-
-
-<!--see-->
-<!--https://sveltestrap.js.org/?path=/docs/components-navbar&#45;&#45;docs-->
-
-<Navbar color="dark" dark expand="md" container="md">
-    <NavbarBrand href="/">
-        <img src={logo} alt="module logo" width="100rem" />
-        My Great Website !
-    </NavbarBrand>
-
-    <NavbarToggler on:click={handleNavClick} />
-
-    <Collapse {isOpen} navbar expand="md">
-<!--        <Collapse {isOpen} navbar expand="md" on:update={handleUpdate}>-->
-        <Nav class="ms-auto" navbar>
-            <NavItem>
-                <NavLink href="#components/">Components</NavLink>
-            </NavItem>
-            <NavItem>
-                <NavLink href="https://github.com/sveltestrap/sveltestrap">GitHub</NavLink>
-            </NavItem>
-            <Dropdown nav inNavbar>
-                <DropdownToggle nav caret>Options</DropdownToggle>
-                <DropdownMenu end>
-                    <DropdownItem>Option 1</DropdownItem>
-                    <DropdownItem>Option 2</DropdownItem>
-                    <DropdownItem divider />
-                    <DropdownItem>Reset</DropdownItem>
-                </DropdownMenu>
-            </Dropdown>
-        </Nav>
-    </Collapse>
-</Navbar>
-
 
 <!--<div class="container">-->
 <Container>
@@ -86,7 +29,7 @@
                 <h2>User details</h2>
                 <FormGroup class="--bs-primary-border-subtle">
                     <Label for="form1_username">Username</Label>
-                    <Input type="text" class="form-control" id="form1_username" aria-describedby="emailHelp" placeholder="Enter email"/>
+                    <Input type="text" class="form-control" id="form1_username" aria-describedby="emailHelp" placeholder="Enter username"/>
                 </FormGroup>
 
                 <Button id="form1_email" type="submit" color="primary" on:click={handleForm1ButtonClick}>
